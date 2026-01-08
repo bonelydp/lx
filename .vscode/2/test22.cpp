d1290c773f1701c80493b54a3033129209a2552f
bonely <g1517227060@163.com>
2026-01-08 16:34:45 +0800

    1

---
+++ .vscode/2/test22.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a;
+    cin >> a;
+    cout << "hello world33333333333" << endl;
+    cout << "hello world33333333333" << endl;
+    return 0;
+}
---
+++ .vscode/lianxi/test2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a;
+    cin >> a;
+    cout << "hello world2222222" << endl;
+    return 0;
+}
